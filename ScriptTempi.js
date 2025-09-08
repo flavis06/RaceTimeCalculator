587c1f26191128d78b214fe650e492759cfb2db4
flavis06 <flaviosorbo06@gmail.com>
2025-09-08 12:28:29 +0200

    Add files via upload

--- ScriptTempi.js
+++ ScriptTempi.js
@@ -1,34 +1,56 @@
-let Time100m = document.getElementById("100").cells[1];
-let Time200m = document.getElementById("200").cells[1];
-let Time400m = document.getElementById("400").cells[1];
-let Time800m = document.getElementById("800").cells[1];
-let Time1000m = document.getElementById("1000").cells[1];
-let Time1500m = document.getElementById("1500").cells[1];
-let Time3000m = document.getElementById("3000").cells[1];
-let Time5000m = document.getElementById("5000").cells[1];
-let Time10000m = document.getElementById("10000").cells[1];
-
-let Time100mConRecord = document.getElementById("100m").cells[1];
-let Time200mConRecord = document.getElementById("200m").cells[1];
-let Time400mConRecord = document.getElementById("400m").cells[1];
-let Time800mConRecord = document.getElementById("800m").cells[1];
-let Time1000mConRecord = document.getElementById("1000m").cells[1];
-let Time1500mConRecord = document.getElementById("1500m").cells[1];
-let Time3000mConRecord = document.getElementById("3000m").cells[1];
-let Time5000mConRecord = document.getElementById("5000m").cells[1];
-let Time10000mConRecord = document.getElementById("10000m").cells[1];
-
 let records;
-
 let k;
-
 let womenRecords =  {"100m": "10.49", "200m": "21.34", "400m": "47.60", "800m": "1:53.28", "1000m": "2:28.98", "1500m": "3:48.68", "3000m": "8:06.11", "5000m": "13:58.06", "10000m": "28:54.14",}
-
 let menRecords = {"100m": "9.58", "200m": "19.19", "400m": "43.03", "800m": "1:40.91", "1000m": "2:11.96", "1500m": "3:26.00", "3000m": "7:17.55", "5000m": "12:35.36", "10000m": "26:11.00",}
+let distances = [100, 200, 400, 800, 1000, 1500, 3000, 5000, 10000]
 
-function allowedTimeValue(tempoCheck) {
-if (tempoCheck.includes(" ") || !(isFinite(Number(tempoCheck.replaceAll(":", "1").replaceAll(".", "1")))))
-	alert("Inserire un valore di tempo valido.");
+function allowedDistanceValue(distanceCheck) {
+if (distanceCheck.includes(" ") || !isFinite(Number(distanceCheck)))
+	alert("Inserire una distanza valida.")
+}
+function checkGender() {
+if (document.querySelector("input[name='sesso2']:checked").value=="male") {
+records = menRecords;
+}
+else {
+records = womenRecords;
+}
+if (document.querySelector("input[name='sesso1']:checked").value=="male") {
+k = 1.123;
+}
+else {
+k = 1.06;
+}
+}
+
+function preliminaryChecks(timeCheck, distanceCheck) {
+let control
+let allowedTimeValue = function(timeCheck) {
+	if (timeCheck.includes(" ") || !(isFinite(Number(timeCheck.replaceAll(":", "1").replaceAll(".", "1"))))) {
+		alert("Inserire un valore di tempo valido.");
+		control = false;
+	}
+}
+let allowedDistanceValue = function(distanceCheck) {
+	if (distanceCheck.includes(" ") || !isFinite(Number(distanceCheck))) {
+		alert("Inserire una distanza valida.");
+		control = false;
+	}
+}
+let checkGender = function() {
+	if (document.querySelector("input[name='sesso2']:checked").value=="male") {
+	records = menRecords;
+	}
+	else {
+	records = womenRecords;
+	}
+	if (document.querySelector("input[name='sesso1']:checked").value=="male") {
+	k = 1.123;
+	}
+	else {
+	k = 1.06;
+}}
+if (!control) return false
 }
 
 function ConvertiTempiInSecondi(tempo) {
@@ -37,19 +59,19 @@ if (tempoArr.length == 3) {
 	let ore = +tempoArr[0];
 	let minuti = +tempoArr[1];
 	let secondi = +tempoArr[2];
-	let t1 = 3600*ore + 60*minuti + secondi;
-	return t1;
+	let tempoInSecondi = 3600*ore + 60*minuti + secondi;
+	return tempoInSecondi;
 	}
 else if (tempoArr.length == 2) {
 	let minuti = +tempoArr[0];
 	let secondi = +tempoArr[1];
-	let t1 = 60*minuti + secondi;
-	return t1;
+	let tempoInSecondi = 60*minuti + secondi;
+	return tempoInSecondi;
 	}
 else if (tempoArr.length == 1) {
 	let secondi = +tempoArr[0];
-	let t1 = secondi;
-	return t1;
+	let tempoInSecondi = secondi;
+	return tempoInSecondi;
 	}
 else alert("Inserire un valore di tempo valido.");
 }
@@ -80,68 +102,38 @@ function ConvertiTempiInStandard(t2) {
 }
 
 function timeCalculator1(d1, t1, d2, k) {
-let t = ((d2/d1)**k)*t1*100;
-let t2 = (t.toFixed(0))*0.01;
-return ConvertiTempiInStandard(t2);
+let t2Raw = (+(((d2/d1)**k)*t1*100).toFixed(0))*0.01;
+return ConvertiTempiInStandard(t2Raw);
 }
 
-
-function calcolaTempi() {
-checkGender();
-let CheckTime = document.getElementById("t1").value.trim();
-let CheckDistance =
-allowedTimeValue(CheckTime);
-let tempo = document.getElementById("t1").value;
-let t1 = ConvertiTempiInSecondi(tempo);
-let d1 = Number(document.getElementById("d1").value);
-Time100m.innerHTML = timeCalculator1(d1, t1, 100, k);
-Time200m.innerHTML = timeCalculator1(d1, t1, 200, k);
-Time400m.innerHTML = timeCalculator1(d1, t1, 400, k);
-Time800m.innerHTML = timeCalculator1(d1, t1, 800, k);
-Time1000m.innerHTML = timeCalculator1(d1, t1, 1000, k);
-Time1500m.innerHTML = timeCalculator1(d1, t1, 1500, k);
-Time3000m.innerHTML = timeCalculator1(d1, t1, 3000, k);
-Time5000m.innerHTML = timeCalculator1(d1, t1, 5000, k);
-Time10000m.innerHTML = timeCalculator1(d1, t1, 10000, k);
-}
-
-function calcolaTempiConRecord() {
-checkGender();
-let distanza = document.getElementById("distanza").value;
-let t0 = document.getElementById("tempo").value;
-allowedTimeValue(t0);
-let t0Corretto = ConvertiTempiInSecondi(t0);
-let tRecord = ConvertiTempiInSecondi(records[distanza]);
-let p = tRecord/t0Corretto;
-Time100mConRecord.innerHTML = timeCalculator2(p, records["100m"]);
-Time200mConRecord.innerHTML = timeCalculator2(p, records["200m"]);
-Time400mConRecord.innerHTML = timeCalculator2(p, records["400m"]);
-Time800mConRecord.innerHTML = timeCalculator2(p, records["800m"]);
-Time1000mConRecord.innerHTML = timeCalculator2(p, records["1000m"]);
-Time1500mConRecord.innerHTML = timeCalculator2(p, records["1500m"]);
-Time3000mConRecord.innerHTML = timeCalculator2(p, records["3000m"]);
-Time5000mConRecord.innerHTML = timeCalculator2(p, records["5000m"]);
-Time10000mConRecord.innerHTML = timeCalculator2(p, records["10000m"]);
-document.getElementById("percentualeRecord").innerHTML = `Percentuale del record del mondo: ${(100*p).toFixed(1)}%`
-}
-
-function timeCalculator2(p, T) {
-let TCorretto = ConvertiTempiInSecondi(T);
-let tFinale = +(Math.trunc((TCorretto/p)*100)*0.01).toFixed(2);
+function timeCalculator2(p, t) {
+let tCorretto = ConvertiTempiInSecondi(t);
+let tFinale = +(Math.trunc((tCorretto/p)*100)*0.01).toFixed(2);
 return ConvertiTempiInStandard(tFinale);
 }
 
-function checkGender () {
-if (document.querySelector("input[name='sesso2']:checked").value=="male") {
-records = menRecords;
-}
-else {
-records = womenRecords;
-}
-if (document.querySelector("input[name='sesso1']:checked").value=="male") {
-k = 1.123;
-}
-else {
-k = 1.06;
-}
+function calcolaTempi() {
+preliminaryChecks(document.getElementById("t1").value.trim(), document.getElementById("d1").value.trim());
+
+let t1 = ConvertiTempiInSecondi(document.getElementById("t1").value.trim());
+let d1 = Number(document.getElementById("d1").value.trim());
+
+distances.forEach(d2 => {
+	let time = document.getElementById(d2.toString()).cells[1];
+	time.innerHTML = timeCalculator1(d1, t1, d2, k);
+)}}
+
+function calcolaTempiConRecord() {
+preliminaryChecks(document.getElementById("tempo").value.trim(), "0");
+
+let t0 = ConvertiTempiInSecondi(document.getElementById("tempo").value.trim());
+let tRecord = ConvertiTempiInSecondi(records[document.getElementById("distanza").value]);
+let p = tRecord/t0;
+
+distances.forEach(d2WithRecord => {
+	let TimeWithRecords = document.getElementById(d2WithRecord.toString() + "m").cells[1];
+	TimeWithRecords.innerHTML = timeCalculator2(p, records[d2WithRecord.toString() + "m"]);
+	})
+
+document.getElementById("percentualeRecord").innerHTML = `Percentuale del record del mondo: ${(100*p).toFixed(1)}%`
 }
